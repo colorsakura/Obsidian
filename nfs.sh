8b1b6d2db0999588711fbad4fd24a887830ed1b9
chauncey <2268136725@qq.com>
2022-10-24 20:31:17 +0800

    update

--- nfs.sh
+++
@@ -1,3 +0,0 @@
-sudo mount -t nfs -o vers=3,nolock,proto=tcp,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 nas.52bilibili.com:/mnt/pool/video/chauncey /home/chauncey/Videos
-
-sudo mount -t nfs -o vers=3,nolock,proto=tcp,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 nas.52bilibili.com:/mnt/pool/file /backup
